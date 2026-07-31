e69b3b5894e2406e8e9949e9c3ba4ef263d111c6
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-31 15:54:35 +0500

    first commit -backend

---
+++ melashorbackend/src/main/java/com/app/melashor/MelashorApplication.java
@@ -0,0 +1,13 @@
+package com.app.melashor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelashorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelashorApplication.class, args);
+	}
+
+}
---
+++ melashorbackend/src/test/java/com/app/melashor/MelashorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.melashor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelashorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
