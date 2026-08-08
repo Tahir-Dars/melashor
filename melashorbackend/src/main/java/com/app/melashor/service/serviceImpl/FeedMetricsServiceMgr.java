8f6e1d7bd456ebebc449a547315b322cafcd3e7f
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-08 22:53:09 +0500

    added these both to use Prometheus with Grafana

---
+++ melashorbackend/src/main/java/com/app/melashor/service/serviceImpl/FeedMetricsServiceMgr.java
@@ -0,0 +1,8 @@
+package com.app.melashor.service.serviceImpl;
+
+import com.app.melashor.service.FeedMetricsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedMetricsServiceMgr implements FeedMetricsService {
+}
