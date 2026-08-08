1668a7514a146a31cdbf27895fcce8c939453c28
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-08 22:49:57 +0500

    implemented the FollowRelationships table to save follower <-> followed relationship

--- melashorbackend/src/main/java/com/app/melashor/domain/model/FollowRelationships.java
+++ melashorbackend/src/main/java/com/app/melashor/domain/model/FollowRelationships.java
@@ -1,13 +1,32 @@
 package com.app.melashor.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "follow_relations")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FollowRelationships {
+    @EmbeddedId
+    private FollowRelationshipsId id;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("followerId")
+    @JoinColumn(name = "follower_id", nullable = false)
+    private UserProfile follower;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("followedId")
+    @JoinColumn(name = "followed_id", nullable = false)
+    private UserProfile followed;
+
+    public FollowRelationships(UserProfile follower,UserProfile followed){
+        this.id=new FollowRelationshipsId(follower.getUserId(),followed.getUserId());
+        this.follower=follower;
+        this.followed=followed;
+    }
 }
+
