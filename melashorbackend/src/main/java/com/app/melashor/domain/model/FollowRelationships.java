52be5ce9c72223e9ee89bb2087f78327e5759874
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-08 19:00:02 +0500

    added the FollowRelationships table to save follower <-> followed relationship

---
+++ melashorbackend/src/main/java/com/app/melashor/domain/model/FollowRelationships.java
@@ -0,0 +1,13 @@
+package com.app.melashor.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "follow_relations")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FollowRelationships {
+
+}
