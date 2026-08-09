a3ee9c547c638fd64d0c6b9029a0efb4d4ec0e15
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-09 12:16:59 +0500

    added FeedCacheServiceMgr class with Spring annotations and default page size constant

--- melashorbackend/src/main/java/com/app/melashor/service/serviceImpl/FeedCacheServiceMgr.java
+++ melashorbackend/src/main/java/com/app/melashor/service/serviceImpl/FeedCacheServiceMgr.java
@@ -1,4 +1,10 @@
 package com.app.melashor.service.serviceImpl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class FeedCacheServiceMgr {
+    public static final int DEFAULT_PAGE_SIZE = 5;
 }
