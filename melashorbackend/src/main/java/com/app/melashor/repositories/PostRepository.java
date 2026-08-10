37d46c0a6db06cffe98c5fb9307286cd6277b7e1
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-10 10:31:56 +0500

    added method to PostRepository for retrieving posts by multiple author IDs in descending order

--- melashorbackend/src/main/java/com/app/melashor/repositories/PostRepository.java
+++ melashorbackend/src/main/java/com/app/melashor/repositories/PostRepository.java
@@ -4,6 +4,10 @@ import com.app.melashor.domain.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, String> {
+    List<Post> findByAuthor_IdInOrderByCreatedAtDescInDesc(Collection<String> authorIds);
 }
