07c60122768e8a39f5316f8eff3aa7701f85389e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-11 09:56:42 +0500

    name field added in the model

--- melashorbackend/src/main/java/com/app/melashor/domain/model/UserProfile.java
+++ melashorbackend/src/main/java/com/app/melashor/domain/model/UserProfile.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,6 +17,9 @@ public class UserProfile {
 
     @Column(unique = true, nullable = false)
     private String handle;
+
+    @Column(unique = true, nullable = false)
+    private String name;
 
     @Column(nullable = false, length = 500)
     private String profileBio;
