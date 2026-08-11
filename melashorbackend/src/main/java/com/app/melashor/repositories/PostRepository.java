8eabd749bb6969fbcd21a54f6dcf9d455ceb8351
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-08-11 19:21:31 +0500

    added method to retrieve posts by author ID and improve query formatting

--- melashorbackend/src/main/java/com/app/melashor/repositories/PostRepository.java
+++ melashorbackend/src/main/java/com/app/melashor/repositories/PostRepository.java
@@ -19,13 +19,16 @@ public interface PostRepository extends JpaRepository<Post, String> {
     List<Post> findByAuthor_IdInOrderByCreatedAtDesc(Collection<String> authorIds, Pageable pageable);
 
     @EntityGraph(attributePaths = "author")
-        @Query("""
-                 select p from Post p
-                             where p.author.userId in:authorIds
-                                         and(p.createdAt<:createdAt or (p.createdAt)=:createdAt
-                                                     and p.postId< :postId )
-                                                                 order by p.createdAt desc, p.postId desc
-                """)
+    List<Post> findByAuthor_IdOrderByCreatedAtDesc(String authorId, Pageable pageable);
+
+    @EntityGraph(attributePaths = "author")
+    @Query("""
+             select p from Post p
+                         where p.author.userId in:authorIds
+                                     and(p.createdAt<:createdAt or (p.createdAt)=:createdAt
+                                                 and p.postId< :postId )
+                                                             order by p.createdAt desc, p.postId desc
+            """)
     List<Post> findHomeFeedPageAfterCursor(@Param("authorIds") Set<String> authorIds,
                                            @Param("createdAt") Instant createdAt,
                                            @Param("postId") String postId,
